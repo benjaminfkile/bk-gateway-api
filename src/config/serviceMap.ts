59edd8f360df141ab994e1b3d01ec8b481e5bc81
Benjamin Kile <benjaminfkile@gmail.com>
2026-03-01 11:47:46 -0600

    add 3gixhub-api to config

--- src/config/serviceMap.ts
+++ src/config/serviceMap.ts
@@ -6,27 +6,54 @@ const baseServices: Record<
   string,
   { port: number; includeInHealthCheck: boolean; includeDevApi?: boolean }
 > = {
-  "portfolio-api": { port: 3001, includeInHealthCheck: true},
-  // "bengrok-api": { port: 3002, includeInHealthCheck: false },
-  "wmsfo-api": { port: 3003, includeInHealthCheck: true, includeDevApi: true  }
+  "portfolio-api": {
+    port: 3001,
+    includeInHealthCheck: true,
+    includeDevApi: false,
+  },
+  // "bengrok-api": {
+  //   port: 3002,
+  //   includeInHealthCheck: false,
+  //   includeDevApi: false,
+  // },
+  "wmsfo-api": {
+    port: 3003,
+    includeInHealthCheck: true,
+    includeDevApi: true,
+  },
+  "3gixhub-api": {
+    port: 3004,
+    includeInHealthCheck: true,
+    includeDevApi: true,
+  },
 };
 
 export const serviceMap: Record<
   string,
   { url: string; includeInHealthCheck: boolean }
 > = Object.fromEntries(
-  Object.entries(baseServices).flatMap(([name, { port, includeInHealthCheck, includeDevApi }]) => {
-    const normalUrl = local ? `http://localhost:${port}` : `http://${name}:${port}`;
-    const entries: [string, { url: string; includeInHealthCheck: boolean }][] = [
-      [name, { url: normalUrl, includeInHealthCheck }],
-    ];
+  Object.entries(baseServices).flatMap(
+    ([name, { port, includeInHealthCheck, includeDevApi }]) => {
+      const normalUrl = local
+        ? `http://localhost:${port}`
+        : `http://${name}:${port}`;
+      const entries: [
+        string,
+        { url: string; includeInHealthCheck: boolean },
+      ][] = [[name, { url: normalUrl, includeInHealthCheck }]];
 
-    if (includeDevApi) {
-      const devPort = port + 1000;
-      const devUrl = local ? `http://localhost:${devPort}` : `http://${name}-dev:${devPort}`;
-      entries.push([`${name}-dev`, { url: devUrl, includeInHealthCheck: false }]);
-    }
+      if (includeDevApi) {
+        const devPort = port + 1000;
+        const devUrl = local
+          ? `http://localhost:${devPort}`
+          : `http://${name}-dev:${devPort}`;
+        entries.push([
+          `${name}-dev`,
+          { url: devUrl, includeInHealthCheck: false },
+        ]);
+      }
 
-    return entries;
-  })
+      return entries;
+    },
+  ),
 );
